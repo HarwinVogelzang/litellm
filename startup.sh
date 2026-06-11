f88d22104d2b749158b420075c7d7c7da4985001
HarwinVogelzang <h.vogelzang@provinciegroningen.nl>
2026-06-11 15:01:29 +0200

    Enhance gunicorn startup options in startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-gunicorn litellm.proxy.proxy_server:app --bind=0.0.0.0:$PORT
+gunicorn litellm.proxy.proxy_server:app \
+  --bind=0.0.0.0:$PORT \
+  --workers=1 \
+  --timeout=600 \
+  --env LITELLM_CONFIG=/home/site/wwwroot/config.yaml
